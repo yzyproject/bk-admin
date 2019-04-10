79f5e2e36d1d5adc40d3ef84e30cc93bcfa593fc
yumeng <2250246493@qq.com>
2019-04-10 23:09:20 +0800

    打包发布

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-export default  {
-    host:"http://106.12.97.169:3001",
-    share:"http://106.12.97.169:3000"
-}
 //生产环境
+export default  {
+    host:"http://149.129.100.214:3001",
+    share:"http://149.129.100.214:3000"
+}
+// 开发环境
 // export default  {
 //     host:"http://localhost:3001",
 //     share:"http://localhost:3000"
